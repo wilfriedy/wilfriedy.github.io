bf25b6acb1448de31b0f585051a49d70e7e5e582
wilfriedy <wilfriedakpe3@gmail.com>
2024-09-20 02:23:50 +0100

    projects detailed description

--- projects.js
+++ projects.js
@@ -3,44 +3,76 @@ export const projectsData = [
     id: 0,
     description: `<h2 class="welcome-hero">PageMentor</h2>
           <p class="welcome-paragraph">
-            I create software solutions that address complex business needs,
-            working collaboratively to enhance project outcomes. My portfolio
-            highlights my journey through continuous learning and effective
-            problem-solving. Check out my work to see how I apply my skills to
-            deliver impactful results.
-          </p>`,
+           <p class="project-des-brief">A Lightweight Website Builder</p>
+          <br>
+          <b>Overview:</b>
+          <br>
+          PagieMentor is a website builder developed with pure JavaScript, TypeScript, and CSS, without using any frameworks. This ensures high performance and flexibility.
+
+            <ul class="features-listing-des">
+                <li>◻
+                  Responsive Design: Optimized for all devices.
+                </li>
+                <li>◻
+                  Drag-and-Drop Interface: Simplifies website creation.
+                </li>
+                <li>◻
+                  Modular Structure: Facilitates easy updates and scalability.
+                </li>
+            </ul>
+
+            Status: Currently under development with ongoing enhancements. ⚠
+          </p>
+          
+          `,
   },
   {
     id: 1,
-    description: `<h2 class="welcome-hero">Giftypal</h2>
+    description: `<h2 class="welcome-hero">Productivity tool</h2>
           <p class="welcome-paragraph">
-            I create software solutions that address complex business needs,
-            working collaboratively to enhance project outcomes. My portfolio
-            highlights my journey through continuous learning and effective
-            problem-solving. Check out my work to see how I apply my skills to
-            deliver impactful results.
+           <p class="project-des-brief">Enhanced Productivity for Developers</p>
+          <br>
+          <b>Overview:</b>
+          <br>
+          This productivity tool streamlines project management inspired by platforms like Linear, adding features like automated commit message generation to accelerate reporting for developers.
+            <ul class="features-listing-des">
+                <li>◻
+                  Task Management: Organize and prioritize tasks efficiently.
+                </li>
+                <li>◻
+                  Automated Commit Messages: Generates commit messages for tasks developement environments.
+                </li>
+                <li>◻
+                 Collaboration Tools: Real-time updates and workflow integration.
+                </li>
+            </ul>
+
+            Status: Currently under development with ongoing enhancements. ⚠
           </p>`,
   },
   {
     id: 2,
-    description: `<h2 class="welcome-hero">Web Packages</h2>
+    description: `<h2 class="welcome-hero">BookmarkerFS</h2>
           <p class="welcome-paragraph">
-            I create software solutions that address complex business needs,
-            working collaboratively to enhance project outcomes. My portfolio
-            highlights my journey through continuous learning and effective
-            problem-solving. Check out my work to see how I apply my skills to
-            deliver impactful results.
-          </p>`,
-  },
-  {
-    id: 3,
-    description: `<h2 class="welcome-hero">File Explorer Bookmarks</h2>
-          <p class="welcome-paragraph">
-            I create software solutions that address complex business needs,
-            working collaboratively to enhance project outcomes. My portfolio
-            highlights my journey through continuous learning and effective
-            problem-solving. Check out my work to see how I apply my skills to
-            deliver impactful results.
+           <p class="project-des-brief">Efficient File Management Desktop Application</p>
+          <br>
+          <b>Overview:</b>
+          <br>
+Bookmarker helps organize files and bookmark folder locations for easy access, enhancing productivity by reducing search time.
+
+            <ul class="features-listing-des">
+                <li>◻
+                 File Organization: Arrange files into customizable categories.
+                </li>
+                <li>◻
+                  Folder Bookmarking: Quick access to frequently used folders.
+                </li>
+                <li>◻
+                  Advanced Search: Fast retrieval of files and folders.
+                </li>
+            </ul>
+
+            Status: Currently under development with ongoing enhancements. ⚠
           </p>`,
   },
 ];
