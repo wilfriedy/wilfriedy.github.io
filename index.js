f1259bae8b8a19963fd95cbb581a6ac48d3d7bed
wilfriedy <wilfriedakpe3@gmail.com>
2025-05-20 01:20:35 +0100

    added carrousel movements

---
+++ index.js
@@ -0,0 +1,45 @@
+import { experiences } from "./options-object-list.js";
+const stepsTrackerDisplay = document.querySelector(".steps-track");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const boxContent = document.querySelector(".experiences-box");
+let startExpIndx = 0;
+
+//dddd
+const handleTrackerCountDisplay = (value) => {
+  stepsTrackerDisplay.textContent = `${value + 1}/${experiences.length}`;
+};
+
+handleTrackerCountDisplay(startExpIndx);
+const handleSetExperienceContent = (step) => {
+  const { company_name, role, year, description, website } = experiences[step];
+  boxContent.innerHTML = `
+            <div class="box-content-top">
+              <div class="">
+                <h3 class="company-name">${company_name}</h3>
+                <a href="${website}">Website</a>
+              </div>
+              <p class="experience-period">${year}</p>
+            </div>
+            <p class="experience-title">${role}</p>
+            <div class="box-content-boddy">
+            ${description}
+            </div>
+  `;
+};
+
+handleSetExperienceContent(startExpIndx);
+
+nextBtn.addEventListener("click", (e) => {
+  if (startExpIndx === experiences.length - 1) return;
+  startExpIndx += 1;
+  handleTrackerCountDisplay(startExpIndx);
+  handleSetExperienceContent(startExpIndx);
+});
+
+prevBtn.addEventListener("click", (e) => {
+  if (startExpIndx < 1) return;
+  startExpIndx -= 1;
+  handleTrackerCountDisplay(startExpIndx);
+  handleSetExperienceContent(startExpIndx);
+});
---
+++ options-object-list.js
@@ -0,0 +1,30 @@
+export const experiences = [
+  {
+    company_name: "Reshiito",
+    role: "Lead software engineer",
+    year: "2023-",
+    website: "reshiito.com",
+    description: ` <p>At reshiito, I worked as a fullstack engineer and helped develop</p> `,
+  },
+  {
+    company_name: "Chapelhive",
+    role: "Frontend developer",
+    year: "Aug 2023-",
+    website: "chapelhive.com",
+    description: ` <p>At reshiito, I worked as a fullstack engineer and helped develop</p> `,
+  },
+  {
+    company_name: "Gems consulting",
+    role: "Technical support",
+    year: "2023-",
+    website: "reshiito.com",
+    description: ` <p>At reshiito, I worked as a fullstack engineer and helped develop</p> `,
+  },
+  {
+    company_name: "British Business Group Nigeria",
+    role: "Web developer",
+    year: "2023-",
+    website: "reshiito.com",
+    description: ` <p>At reshiito, I worked as a fullstack engineer and helped develop</p> `,
+  },
+];
